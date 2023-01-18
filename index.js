b7c4612a316e1dfbb901118454c0c538f65113e1
Thomas Vargas <nolanvargas@byui.edu>
2023-01-17 23:16:41 -0700

    Imported files

---
+++ index.js
@@ -0,0 +1,17 @@
+import sites from "/sites.json";
+
+function buildSiteTemplate(name, description) {}
+
+function displayHomeMenu() {
+  content = document.querySelector(".content");
+}
+
+function loadHomeMenu() {}
+
+function main() {
+  loadHomeMenu();
+  displayHomeMenu();
+  console.log(sites);
+}
+
+document.addEventListener("DOMContentLoaded", main);
