1429de91b883883ac24f7e24a8e95253ac370cad
Thomas <nolanvargas@byui.edu>
2023-01-18 00:36:05 -0700

    added functionality for creating site cards

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
-import sites from "/sites.json";
+import data from "./sites.json" assert { type: "json" };
 
-function buildSiteTemplate(name, description) {}
-
-function displayHomeMenu() {
-  content = document.querySelector(".content");
+function buildSiteTemplate(name, data) {
+  console.log(name, data);
+  const html = `<div class="siteCard"><div class="siteContent"><h1>${data.title}</h1></div></div>`;
+  return html;
 }
 
-function loadHomeMenu() {}
+function displayHomeMenu(homeHTML) {
+  const content = document.querySelector(".content");
+  content.innerHTML = homeHTML.join("");
+}
 
-function main() {
-  loadHomeMenu();
-  displayHomeMenu();
-  console.log(sites);
+function loadHomeHTML() {
+  let homeHTML = [];
+  Object.entries(data).forEach(([key, value]) => {
+    const siteHTML = buildSiteTemplate(key, value);
+    homeHTML.push(siteHTML);
+  });
+  return homeHTML;
+}
+
+async function main() {
+  const homeHTML = loadHomeHTML();
+  displayHomeMenu(homeHTML);
 }
 
 document.addEventListener("DOMContentLoaded", main);
