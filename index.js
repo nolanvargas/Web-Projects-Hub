1e9a55961da1ffd27fb894f1a8ab408b0623fd5e
Thomas Vargas <43393506+nolanvargas@users.noreply.github.com>
2023-01-22 21:05:27 -0700

    Ready to go live

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-import data from "./sites.json" assert { type: "json" };
-
 function buildSiteTemplate(name, data) {
-  const html = `<div class="siteCard">
+  const html = `<div class="siteCard" id="${name}">
                   <div class="siteContent">
                     <h1>${data.title}</h1>
                     <p>${data.description}</p>
@@ -15,7 +13,7 @@ function displayHomeMenu(homeHTML) {
   content.innerHTML = homeHTML.join("");
 }
 
-function loadHomeHTML() {
+function loadHomeHTML(data) {
   let homeHTML = [];
   Object.entries(data).forEach(([key, value]) => {
     const siteHTML = buildSiteTemplate(key, value);
@@ -24,9 +22,46 @@ function loadHomeHTML() {
   return homeHTML;
 }
 
+function loadClickListener(data) {
+  const siteCards = document.querySelectorAll(".siteCard");
+  siteCards.forEach((card) => {
+    card.addEventListener("click", (event) => {
+      const id = card.id;
+      // runPerformanceTest(data[id].link, card);
+      window.open(data[id].link, "_blank");
+    });
+  });
+}
+
+function runPerformanceTest(url, siteCard) {
+  performance.clearMarks();
+  performance.clearMeasures();
+  performance.mark("startNavigation");
+  fetch(url)
+    .then(() => {
+      performance.mark("endNavigation");
+      performance.measure("performance", "startNavigation", "endNavigation");
+      const measure = performance.getEntriesByName("performance")[0];
+      const loadTime = measure.duration;
+      const childNode = `<div class='metrics'><div class="load-time">${loadTime}ms</div>`;
+      siteCard.innerHTML += childNode;
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
 async function main() {
-  const homeHTML = loadHomeHTML();
-  displayHomeMenu(homeHTML);
+  let sites = {};
+  fetch("./sites.json")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      const homeHTML = loadHomeHTML(data);
+      displayHomeMenu(homeHTML);
+      loadClickListener(data);
+    });
 }
 
 document.addEventListener("DOMContentLoaded", main);
